4d6be1bf852ceff87cbcadc706e60da8c30f9d7e
daniil <smokequitz@gmail.com>
2023-01-08 20:57:47 +0000

    Added some new files and ignore file

--- To_do_list_web_app.py
+++ To_do_list_web_app.py
@@ -14,8 +14,13 @@ st.title('My To-do App')
 st.subheader('This is my todo app!')
 st.write('This app will increase your productivity')
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        funtions.write_file(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
-st.text_input(label='Enter a todo', placeholder='add new todo...',
+st.text_input(label='Enter a todo', placeholder='Add new todo...',
               on_change=add_todo, key='new_todo')

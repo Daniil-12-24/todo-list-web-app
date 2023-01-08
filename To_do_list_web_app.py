e498715c57d790d50e2c18f044208798cf883664
daniil <smokequitz@gmail.com>
2023-01-08 20:33:04 +0000

    Implemented 'Add new todo'

--- To_do_list_web_app.py
+++ To_do_list_web_app.py
@@ -3,6 +3,13 @@ import funtions
 
 todos = funtions.read_file()
 
+
+def add_todo():
+    todo = st.session_state['new_todo'] + '\n'
+    todos.append(todo)
+    funtions.write_file(todos)
+
+
 st.title('My To-do App')
 st.subheader('This is my todo app!')
 st.write('This app will increase your productivity')
@@ -10,4 +17,5 @@ st.write('This app will increase your productivity')
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label='Enter a todo', placeholder='add new todo...')
+st.text_input(label='Enter a todo', placeholder='add new todo...',
+              on_change=add_todo, key='new_todo')
